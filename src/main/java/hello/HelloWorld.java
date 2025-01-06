d19f96dc7eb73bd1c548683c58fb4baf34b3b429
Abishek <82356406+abishek131294@users.noreply.github.com>
2025-01-06 11:05:03 +0530

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,6 +2,6 @@ package hello;
 
 public class HelloWorld {
     public static String getMessage() {
-        return "Hello, World3.1_server1312! v0.2.6";
+        return "Hello_monday_morning_test, World3.1_server1312! v0.2.6";
     }
 }
